7b0757fe3c53dc1673ade9bd520243d47ea1c317
Gokul Viswanathan <gokul2594@gmail.com>
2023-06-12 15:18:50 -0400

    logic added to count files on the dorectory script is run

--- gokul-countlines.sh
+++ gokul-countlines.sh
@@ -2,22 +2,53 @@
 
 echo "countig lines...."
 
+owner=""
+month=""
+
 help_message() {
     echo "invalid command: no parameter included with argument $OPTARG"
     echo "valid arguments format"
     echo "./gokul-countlines.sh -o owner -m month"
+    echo "month should be of format Jan, Feb, etc.."
+}
+
+shouldGetLines() {
+  fileInfo=`stat --printf "%n|%G|%w|" $1`
+  IFS="|" read -a fileInfoArray <<< $fileInfo
+  MONTH=$(date -d "${fileInfoArray[2]}" '+%b')
+  valid=1
+
+  if [ ${owner^^} = ${fileInfoArray[1]^^} ] && [ ${month^^} = ${MONTH^^} ]; then
+    valid=0
+  fi
+  
+  return $valid
+}
+
+countLines() {
+  shouldGetLines $1
+  valid=$?
+  if [ $valid = 0 ]; then
+    count=`wc -l $1`
+    echo "File: $1, Lines: $count"
+  fi
 }
 
 while getopts ":o:m:" opt; do
   case $opt in
      o)
-       echo "-o parameter $OPTARG" >&2
+       owner=$OPTARG >&2
        ;;
      m)
-       echo "-m parameter $OPTARG" >&2
+       month=$OPTARG >&2
        ;;
      *)
        help_message
        ;;
   esac
+done
+
+
+for FILE in *; do 
+  countLines $FILE;
 done
