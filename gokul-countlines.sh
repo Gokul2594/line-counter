346fe482583f4f81c3e079a51695f4fff5c9b5f7
Gokul Viswanathan <gokul2594@gmail.com>
2023-06-12 16:42:44 -0400

    script updated to accept both args as optional

--- gokul-countlines.sh
+++ gokul-countlines.sh
@@ -2,6 +2,7 @@
 
 owner=""
 month=""
+flow=""
 
 help_message() {
     echo "valid arguments format should be:"
@@ -9,27 +10,74 @@ help_message() {
     echo "month should be of format Jan, Feb, etc.."
 }
 
-shouldGetLines() {
+isValidMonth() {
+  temp=$1
+  validMonth=1
+  if [ ${month^^} = ${temp^^} ]; then
+    validMonth=0
+  fi
+  return $validMonth
+}
+
+isValidOwner() {
+  validOwner=1
+  if [ ${owner^^} = $1 ]; then
+    validOwner=0
+  fi
+  return $validOwner
+}
+
+getLines() {
   fileInfo=`stat --printf "%n|%G|%w|" $1`
   IFS="|" read -a fileInfoArray <<< $fileInfo
   MONTH=$(date -d "${fileInfoArray[2]}" '+%b')
-  valid=1
 
-  if [ ${owner^^} = ${fileInfoArray[1]^^} ] && [ ${month^^} = ${MONTH^^} ]; then
-    valid=0
-  fi
+  if [ $flow = "OWNERMONTH" ]; then
+    isValidMonth $MONTH
+    validatedMonth=$?
+    isValidOwner ${fileInfoArray[1]^^}
+    validatedOwner=$?
+
+    if [ $validatedMonth = 0 ] && [ $validatedOwner = 0 ]; then
+      countLines $1
+    fi
+
+  elif [ $flow = "OWNER" ]; then
+    isValidOwner ${fileInfoArray[1]^^}
+    validatedOwner=$?
+    
+    if [ $validatedOwner = 0 ]; then
+      countLines $1
+    fi
   
-  return $valid
+  else
+    isValidMonth $MONTH
+    validatedMonth=$?
+    if [ $validatedMonth = 0 ]; then
+      countLines $1
+    fi
+  fi
+}
+
+setFlow() {
+  if [ ! -z "$owner" ] && [ ! -z "$month" ]; then
+    echo "Looking for files created in the month: $month by: $owner"
+    flow="OWNERMONTH"
+  elif [ ! -z "$owner" ] && [ -z "$month" ]; then
+    echo "Looking for files created by: $owner"
+    flow="OWNER"
+  else
+    echo "Looking for files created in the month: $month"
+    flow="MONTH"
+  fi
 }
 
 countLines() {
-  shouldGetLines $1
-  valid=$?
-  if [ $valid = 0 ]; then
-    count=`wc -l $1`
-    echo "File: $1, Lines: $count"
-  fi
+  count=`wc -l $1`
+  echo "File: $1, Lines: $count"
 }
+
+
 
 while getopts ":o:m:" opt; do
   case $opt in
@@ -50,8 +98,8 @@ then
     help_message
     exit 0
 else
-  echo "Looking for files created in the month: $month by: $owner"
+  setFlow
   for FILE in *; do
-    countLines $FILE;
+    getLines $FILE
   done
 fi
