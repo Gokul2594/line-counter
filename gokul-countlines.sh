209e4ed9c7e24cd0975ad10f937e54fda655837f
Gokul Viswanathan <gokul2594@gmail.com>
2023-06-12 12:30:55 -0400

    logic to take in o and m args added

--- gokul-countlines.sh
+++ gokul-countlines.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo "Will count lines soon!"
+echo "countig lines...."
+
+help_message() {
+    echo "invalid command: no parameter included with argument $OPTARG"
+    echo "valid arguments format"
+    echo "./gokul-countlines.sh -o owner -m month"
+}
+
+while getopts ":o:m:" opt; do
+  case $opt in
+     o)
+       echo "-o parameter $OPTARG" >&2
+       ;;
+     m)
+       echo "-m parameter $OPTARG" >&2
+       ;;
+     *)
+       help_message
+       ;;
+  esac
+done
